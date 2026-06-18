842a4d747d455bae6efac30c3ed54f811b1ea511
ATRIX08 <renatinhorecanto@gmail.com>
2026-06-18 01:21:43 -0300

    Adicionado cálculo de fatorial e melhorias

--- script.js
+++ script.js
@@ -8,7 +8,41 @@ function fatorial(numero) {
   return resultado;
 }
 
-function calcular() {
+function calcularFatorial() {
+  const nInput = document.getElementById("n");
+  const n = Number(nInput.value);
+
+  const erro = document.getElementById("erro");
+  const resultado = document.getElementById("resultado");
+
+  erro.innerHTML = "";
+  resultado.innerHTML = "";
+
+  if (nInput.value === "") {
+    erro.innerHTML = "Digite um valor para n.";
+    return;
+  }
+
+  if (n < 0) {
+    erro.innerHTML = "Não são permitidos números negativos.";
+    return;
+  }
+
+  if (!Number.isInteger(n)) {
+    erro.innerHTML = "Digite apenas números inteiros.";
+    return;
+  }
+
+  const valorFatorial = fatorial(n);
+
+  resultado.innerHTML = `
+        <h3>Resultado do Fatorial</h3>
+        <br>
+        <p><strong>${n}! = ${valorFatorial}</strong></p>
+    `;
+}
+
+function calcularCombinacao() {
   const nInput = document.getElementById("n");
   const pInput = document.getElementById("p");
 
@@ -22,7 +56,7 @@ function calcular() {
   resultado.innerHTML = "";
 
   if (nInput.value === "" || pInput.value === "") {
-    erro.innerHTML = "Preencha todos os campos.";
+    erro.innerHTML = "Preencha os campos n e p.";
     return;
   }
 
@@ -48,12 +82,23 @@ function calcular() {
   const combinacao = nFatorial / (pFatorial * npFatorial);
 
   resultado.innerHTML = `
-        <h3>Resultado</h3>
+        <h3>Resultado da Combinação</h3>
         <br>
-        <p><strong>${n}!</strong> = ${nFatorial}</p>
-        <p><strong>${p}!</strong> = ${pFatorial}</p>
-        <p><strong>(${n}-${p})!</strong> = ${npFatorial}</p>
+        <p>${n}! = ${nFatorial}</p>
+        <p>${p}! = ${pFatorial}</p>
+        <p>(${n}-${p})! = ${npFatorial}</p>
+
         <br>
-        <p><strong>C(${n},${p}) = ${combinacao}</strong></p>
+
+        <p>
+            C(${n},${p}) =
+            ${nFatorial} / (${pFatorial} × ${npFatorial})
+        </p>
+
+        <br>
+
+        <p>
+            <strong>C(${n},${p}) = ${combinacao}</strong>
+        </p>
     `;
 }
